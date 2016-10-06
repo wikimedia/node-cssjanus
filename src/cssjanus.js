f1f38289b85a0757ff40aa90023b375c85a7bfe4
Yair Rand <yyairrand@gmail.com>
2016-10-06 04:49:30 -0400

    fix trivial whitespace to satisfy the tests

--- src/cssjanus.js
+++ src/cssjanus.js
@@ -327,7 +327,7 @@ function CSSJanus() {
 				noFlipClassTokenizer,
 				commentTokenizer,
 				i;
-			
+
 			// Default values
 			sourceDir = sourceDir || 'lr-tb';
 			targetDir = targetDir || 'rl-tb';
